4fb69a9ea0f1ef98b8d5b7c63024f0cb0d9bc0e1
afeiship <1290657123@qq.com>
2018-09-18 09:18:02 +0800

    issue: default grabbed

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,8 @@
 class Page < ApplicationRecord
+  validates_uniqueness_of :url
+  before_save :default_values
+
+  def default_values
+    self.grabbed ||= false
+  end
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,8 @@
 class Url < ApplicationRecord
+  validates_uniqueness_of :url
+  before_save :default_values
+
+  def default_values
+    self.grabbed ||= false
+  end
 end
